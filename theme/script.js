c454a0d8230d128f29f4e328cbe074164314da20
ToghrulMirzayev <togrul.mirzoev@gmail.com>
2023-05-06 11:57:48 +0200

    themes implemented

---
+++ theme/script.js
@@ -0,0 +1,20 @@
+const toggleTheme = document.querySelector('#toggle-theme');
+const body = document.body;
+const localStorageKey = 'theme';
+
+const currentTheme = localStorage.getItem(localStorageKey);
+
+if (currentTheme) {
+body.classList.add(currentTheme);
+toggleTheme.checked = (currentTheme === 'night-mode');
+}
+
+toggleTheme.addEventListener('change', function() {
+if (this.checked) {
+  body.classList.add('night-mode');
+  localStorage.setItem(localStorageKey, 'night-mode');
+} else {
+  body.classList.remove('night-mode');
+  localStorage.setItem(localStorageKey, 'day-mode');
+}
+});
