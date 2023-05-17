88c09e238b72c50d159312bf06572715528a9057
ToghrulMirzayev <togrul.mirzoev@gmail.com>
2023-05-18 00:46:53 +0200

    New error messages added for empty fields

--- auth/script.js
+++ auth/script.js
@@ -1,16 +1,25 @@
-document.getElementById("username").title = "Enter your username";
-document.getElementById("password").title = "Enter your password";
-
 function validateForm() {
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-if (username === "correct_user" && password === "correct_password") {
-  window.location.href = "hover_and_select.html";
-  return false;
-} else {
-  document.getElementById("message").innerHTML = "Password or username is incorrect";
-  document.getElementById("message").className = "error-message";
-  return false;
-}
+  if (username === "" && password === "") {
+    document.getElementById("message").innerHTML = "Username and password fields cannot be empty";
+    document.getElementById("message").className = "error-message";
+    return false;
+  } else if (username === "") {
+    document.getElementById("message").innerHTML = "Username field cannot be empty";
+    document.getElementById("message").className = "error-message";
+    return false;
+  } else if (password === "") {
+    document.getElementById("message").innerHTML = "Password field cannot be empty";
+    document.getElementById("message").className = "error-message";
+    return false;
+  } else if (username === "correct_username" && password === "correct_password") {
+    window.location.href = "hover_and_select.html";
+    return false;
+  } else {
+    document.getElementById("message").innerHTML = "Password or username is incorrect";
+    document.getElementById("message").className = "error-message";
+    return false;
+  }
 }
